95a39af7f9229b89d741009ba56f7e9f5fb860c0
SammuelLeroux <sammuel.leroux@epitech.eu>
2025-11-20 09:53:40 +0100

    fix: debug ci/cd

--- infra/envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-stg_10"
-    storage_account_name = "tfstatebackendsammuel"
-    container_name       = "tfstate"
-    key                  = "terraform-stg.tfstate"
-  }
-}

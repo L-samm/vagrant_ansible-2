2364fa895513e75c014a325bf24d460e78ff7d38
SammuelLeroux <sammuel.leroux@epitech.eu>
2025-11-20 11:32:21 +0100

    feat: ajout role config pour configurer le swap memoire de 8giga

--- infra/modules/ansible/main.tf
+++ infra/modules/ansible/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 resource "ansible_playbook" "playbook" {
-  playbook   = "${path.module}/../playbook.yml"
+  playbook   = "${path.root}/ansible/playbook.yml"
   name       = "vm"
 
   extra_vars = {

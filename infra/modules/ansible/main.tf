c0000d9d1ad851e1c9334fe4188bd7b3eae800de
SammuelLeroux <sammuel.leroux@epitech.eu>
2025-11-20 11:34:24 +0100

    fix: debug typo

--- infra/modules/ansible/main.tf
+++ infra/modules/ansible/main.tf
@@ -13,7 +13,7 @@ resource "ansible_playbook" "playbook" {
 
   extra_vars = {
     ansible_host                  = var.ip_vm
-    ansible_user                  = "admi_db"
+    ansible_user                  = "admin_db"
     ansible_ssh_private_key_file  = var.ssh_key_path
   }
 
